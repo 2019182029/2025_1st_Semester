31da0ce462c7fd1a5905420ddcdc2015813a3a78
2019182029 <144346191+2019182029@users.noreply.github.com>
2025-05-09 23:38:40 +0900

    5-2

--- 실습 5-2/Project.cpp
+++ 실습 5-2/Project.cpp
@@ -4,8 +4,11 @@
 #include <queue>
 #include <vector>
 #include <random>
+#include <thread>
+#include <iostream>
 #include "resource.h"
-#include "resource1.h"
+
+#pragma comment(linker,"/entry:WinMainCRTStartup /subsystem:console")
 
 HINSTANCE g_hInst;
 LPCTSTR lpszClass = L"Window Class Name";
@@ -57,26 +60,28 @@ public:
 	void set_h(int hx, int hy) { m_hx = hx; m_hy = hy; }
 	void set_m(int mx, int my) { m_mx = mx; m_my = my; }
 
-	void print(HDC hDC, HDC mDC, RECT rect, BITMAP bmp, int division) const {
-		StretchBlt(hDC, 
-			
-			(m_hx * (rect.right / 3)) + m_dx, (m_hy * (rect.bottom / 3)) + m_dy,
-			(rect.right / division), (rect.bottom / division),
-			
-			mDC, 
-			
-			m_mx * (bmp.bmWidth / 3), m_my * (bmp.bmHeight / 3),
-			(bmp.bmWidth / division), (bmp.bmHeight / division), 
-			
-			SRCCOPY);
+	void print(HDC hDC, HDC mDC, RECT rect, BITMAP bmp, int r, int c, bool reverse = false) const {
+		if (!reverse) {
+			StretchBlt(hDC, (m_hx * (rect.right / c)) + m_dx, (m_hy * (rect.bottom / r)) + m_dy,
+				(rect.right / c), (rect.bottom / r),
+				mDC, m_mx * (bmp.bmWidth / c), m_my * (bmp.bmHeight / r),
+				(bmp.bmWidth / c), (bmp.bmHeight / r), SRCCOPY);
+		} else {
+			StretchBlt(hDC, (m_hx * (rect.right / c)) + m_dx, (m_hy * (rect.bottom / r)) + m_dy,
+				(rect.right / c), (rect.bottom / r),
+				mDC, m_mx * (bmp.bmWidth / c), m_my * (bmp.bmHeight / r),
+				(bmp.bmWidth / c), (bmp.bmHeight / r), NOTSRCCOPY);
+		}
 	}
 };
 
+std::random_device rd;
+std::mt19937 g(rd()); 
+
 std::queue<PIECE*> removed_pieces;
 std::vector<std::pair<int, int>> empty_spaces;
-std::array<std::array<PIECE*, 3>, 3> pieces_3x3;
-std::array<std::array<PIECE*, 4>, 4> pieces_4x4;
-std::array<std::array<PIECE*, 5>, 5> pieces_5x5;
+std::vector<std::pair<int, int>> moving_spaces;
+std::vector<std::vector<PIECE*>> pieces;
 
 //////////////////////////////////////////////////
 // WINMAIN
@@ -115,13 +120,95 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE	hPrevInstance, _
 
 //////////////////////////////////////////////////
 // WNDPROC
-int cur_mode = 3;
+int row = 0;
+int col = 0;
 
 bool is_moving = false;
 
 int clicked_x = 0;
 int clicked_y = 0;
 bool clicked = false;
+
+void set_pieces(int r, int c) {
+	row = r;
+	col = c;
+
+	// Clear Containers
+	while (!removed_pieces.empty()) {
+		removed_pieces.pop();
+	}
+
+	empty_spaces.clear();
+	moving_spaces.clear();
+
+	for (auto& r : pieces) {
+		r.clear();
+	}
+	pieces.clear();
+
+	// Set Pieces
+	for (int i = 0; i < row; ++i) {
+		pieces.emplace_back();
+	}
+
+	for (int i = 0; i < col; ++i) {
+		for (int j = 0; j < row; ++j) {
+			pieces[j].emplace_back(new PIECE(i, j));
+		}
+	}
+
+	std::shuffle(pieces.begin(), pieces.end(), g);
+	for (int i = 0; i < row; ++i) {
+		std::shuffle(pieces[i].begin(), pieces[i].end(), g);
+	}
+
+	for (int i = 0; i < col; ++i) {
+		for (int j = 0; j < row; ++j) {
+			pieces[j][i]->set_h(i, j);
+		}
+	}
+}
+
+void start_game() {
+	if ((0 != row) && (0 != col)) {
+		PIECE* piece = pieces[0][0];
+		empty_spaces.emplace_back(std::make_pair(piece->m_hx, piece->m_hy));
+		pieces[0][0] = nullptr;
+		delete piece;
+	}
+}
+
+bool hint = false;
+bool reverse = false;
+bool vh = false;
+PIECE* picked = nullptr;
+
+bool check_win() {
+	int cnt = 0;
+	for (int r = 0; r < row; ++r) {
+		for (int c = 0; c < col; ++c) {
+			if (!pieces[r][c]) {
+				++cnt;
+				continue;
+			}
+
+			if ((pieces[r][c]->m_hx != pieces[r][c]->m_mx) || (pieces[r][c]->m_hy != pieces[r][c]->m_my)) {
+				++cnt;
+			}
+		}
+	}
+
+	if (!vh) {
+		if (1 == cnt) {
+			return true;
+		}
+	} else {
+		if (0 == cnt) {
+			return true;
+		}
+	}
+	return false;
+}
 
 LRESULT WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
 	PAINTSTRUCT ps;
@@ -132,128 +219,276 @@ LRESULT WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
 
 	switch (iMessage) {
 	case WM_CREATE: {
-		hBitmap = (HBITMAP)LoadImage(g_hInst, TEXT("tukorea.bmp"), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+		hBitmap = (HBITMAP)LoadImage(g_hInst, TEXT("kpu.bmp"), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
 		GetClientRect(hWnd, &rect);
 		GetObject(hBitmap, sizeof(BITMAP), &bmp);
-
-		std::random_device rd;
-		std::mt19937 g(rd());
-
-		// Set 3x3 pieces
-		for (int i = 0; i < 3; ++i) {
-			for (int j = 0; j < 3; ++j) {
-				pieces_3x3[j][i] = new PIECE(i, j);
-			}
-		}
-
-		std::shuffle(pieces_3x3.begin(), pieces_3x3.end(), g);
-		for (int i = 0; i < 3; ++i) {
-			std::shuffle(pieces_3x3[i].begin(), pieces_3x3[i].end(), g);
-		}
-
-		for (int i = 0; i < 3; ++i) {
-			for (int j = 0; j < 3; ++j) {
-				pieces_3x3[j][i]->set_h(i, j);
-			}
-		}
-
-		PIECE* piece = pieces_3x3[0][0];
-		empty_spaces.emplace_back(std::make_pair(piece->m_hx, piece->m_hy));
-		pieces_3x3[0][0] = nullptr;
-		delete piece;
 		break;
 	}
 
 	case WM_TIMER:
 		switch (wParam) {
 		case 0:
-			for (auto& p : empty_spaces) {
-				if ((cur_mode - 1) == p.second) { continue; }
-				if (nullptr == pieces_3x3[p.second + 1][p.first]) { continue; }
+			for (auto iter = moving_spaces.begin(); iter != moving_spaces.end();) {
+				if ((row - 1) == iter->second) { 
+					empty_spaces.emplace_back(*iter);
+					iter = moving_spaces.erase(iter);
+					continue; 
+				}
+				
+				if (nullptr == pieces[iter->second + 1][iter->first]) { 
+					empty_spaces.emplace_back(*iter);
+					iter = moving_spaces.erase(iter);
+					continue; 
+				}
+				
+				is_moving = true;
 
-				pieces_3x3[p.second + 1][p.first]->m_dy -= ((rect.bottom / 3) / 16);
+				pieces[iter->second + 1][iter->first]->m_dy -= ((rect.bottom / row) / 16);
 
-				if (abs(pieces_3x3[p.second + 1][p.first]->m_dy) >= (rect.bottom / 3)) {
-					KillTimer(hWnd, 0);
-					is_moving = false;
-					pieces_3x3[p.second + 1][p.first]->m_hy -= 1;
-					pieces_3x3[p.second + 1][p.first]->m_dy = 0;
-					pieces_3x3[p.second][p.first] = pieces_3x3[p.second + 1][p.first];
-					pieces_3x3[p.second + 1][p.first] = nullptr;
-					++p.second;
+				if (abs(pieces[iter->second + 1][iter->first]->m_dy) >= (rect.bottom / row)) {
+					pieces[iter->second + 1][iter->first]->m_hy -= 1;
+					pieces[iter->second + 1][iter->first]->m_dy = 0;
+					pieces[iter->second][iter->first] = pieces[iter->second + 1][iter->first];
+					pieces[iter->second + 1][iter->first] = nullptr;
+
+					iter->second += 1;
+					empty_spaces.emplace_back(*iter);
+
+					iter = moving_spaces.erase(iter);
+					continue;
+				}
+				++iter;
+			}
+
+			if (!moving_spaces.size()) {
+				KillTimer(hWnd, 0);
+				is_moving = false;
+
+				if (check_win()) {
+					InvalidateRect(hWnd, NULL, TRUE);
+					MessageBox(hWnd, L"Game Clear!", L"You Win!", MB_OK | MB_ICONINFORMATION);
+					PostQuitMessage(0);
+					return 0;
 				}
 			}
 			break;
 
 		case 1:
-			for (auto& p : empty_spaces) {
-				if (0 == p.second) { continue; }
-				if (nullptr == pieces_3x3[p.second - 1][p.first]) { continue; }
+			for (auto iter = moving_spaces.begin(); iter != moving_spaces.end();) {
+				if (0 == iter->second) { 
+					empty_spaces.emplace_back(*iter);
+					iter = moving_spaces.erase(iter);
+					continue; 
+				}
+				
+				if (nullptr == pieces[iter->second - 1][iter->first]) { 
+					empty_spaces.emplace_back(*iter);
+					iter = moving_spaces.erase(iter);
+					continue; 
+				}
 
-				pieces_3x3[p.second - 1][p.first]->m_dy += ((rect.bottom / 3) / 16);
+				is_moving = true;
 
-				if (abs(pieces_3x3[p.second - 1][p.first]->m_dy) >= (rect.bottom / 3)) {
-					KillTimer(hWnd, 1);
-					is_moving = false;
-					pieces_3x3[p.second - 1][p.first]->m_hy += 1;
-					pieces_3x3[p.second - 1][p.first]->m_dy = 0;
-					pieces_3x3[p.second][p.first] = pieces_3x3[p.second - 1][p.first];
-					pieces_3x3[p.second - 1][p.first] = nullptr;
-					--p.second;
+				pieces[iter->second - 1][iter->first]->m_dy += ((rect.bottom / row) / 16);
+
+				if (abs(pieces[iter->second - 1][iter->first]->m_dy) >= (rect.bottom / row)) {
+					pieces[iter->second - 1][iter->first]->m_hy += 1;
+					pieces[iter->second - 1][iter->first]->m_dy = 0;
+					pieces[iter->second][iter->first] = pieces[iter->second - 1][iter->first];
+					pieces[iter->second - 1][iter->first] = nullptr;
+					
+					iter->second -= 1;
+					empty_spaces.emplace_back(*iter);
+
+					iter = moving_spaces.erase(iter);
+					continue;
+				}
+				++iter;
+			}
+
+			if (!moving_spaces.size()) {
+				KillTimer(hWnd, 1);
+				is_moving = false;
+
+				if (check_win()) {
+					InvalidateRect(hWnd, NULL, TRUE);
+					MessageBox(hWnd, L"Game Clear!", L"You Win!", MB_OK | MB_ICONINFORMATION);
+					PostQuitMessage(0);
+					return 0;
 				}
 			}
 			break;
 
 		case 2:
-			for (auto& p : empty_spaces) {
-				if ((cur_mode - 1) == p.first) { continue; }
-				if (nullptr == pieces_3x3[p.second][p.first + 1]) { continue; }
+			for (auto iter = moving_spaces.begin(); iter != moving_spaces.end();) {
+				if ((col - 1) == iter->first) { 
+					empty_spaces.emplace_back(*iter);
+					iter = moving_spaces.erase(iter);
+					continue; 
+				}
+				
+				if (nullptr == pieces[iter->second][iter->first + 1]) { 
+					empty_spaces.emplace_back(*iter);
+					iter = moving_spaces.erase(iter);
+					continue; 
+				}
 
-				pieces_3x3[p.second][p.first + 1]->m_dx -= ((rect.right / 3) / 16);
+				is_moving = true;
 
-				if (abs(pieces_3x3[p.second][p.first + 1]->m_dx) >= (rect.right / 3)) {
-					KillTimer(hWnd, 2);
-					is_moving = false;
-					pieces_3x3[p.second][p.first + 1]->m_hx -= 1;
-					pieces_3x3[p.second][p.first + 1]->m_dx = 0;
-					pieces_3x3[p.second][p.first] = pieces_3x3[p.second][p.first + 1];
-					pieces_3x3[p.second][p.first + 1] = nullptr;
-					++p.first;
+				pieces[iter->second][iter->first + 1]->m_dx -= ((rect.right / col) / 16);
+
+				if (abs(pieces[iter->second][iter->first + 1]->m_dx) >= (rect.right / col)) {
+					pieces[iter->second][iter->first + 1]->m_hx -= 1;
+					pieces[iter->second][iter->first + 1]->m_dx = 0;
+					pieces[iter->second][iter->first] = pieces[iter->second][iter->first + 1];
+					pieces[iter->second][iter->first + 1] = nullptr;
+					
+					iter->first += 1;
+					empty_spaces.emplace_back(*iter);
+
+					iter = moving_spaces.erase(iter);
+					continue;
+				}
+				++iter;
+			}
+
+			if (!moving_spaces.size()) {
+				KillTimer(hWnd, 2);
+				is_moving = false;
+
+				if (check_win()) {
+					InvalidateRect(hWnd, NULL, TRUE);
+					MessageBox(hWnd, L"Game Clear!", L"You Win!", MB_OK | MB_ICONINFORMATION);
+					PostQuitMessage(0);
+					return 0;
 				}
 			}
 			break;
 
 		case 3:
-			for (auto& p : empty_spaces) {
-				if (0 == p.first) { continue; }
-				if (nullptr == pieces_3x3[p.second][p.first - 1]) { continue; }
+			for (auto iter = moving_spaces.begin(); iter != moving_spaces.end();) {
+				if (0 == iter->first) {
+					empty_spaces.emplace_back(*iter);
+					iter = moving_spaces.erase(iter);
+					continue; 
+				}
 
-				pieces_3x3[p.second][p.first - 1]->m_dx += ((rect.right / 3) / 16);
+				if (nullptr == pieces[iter->second][iter->first - 1]) {
+					empty_spaces.emplace_back(*iter);
+					iter = moving_spaces.erase(iter);
+					continue; 
+				}
 
-				if (abs(pieces_3x3[p.second][p.first - 1]->m_dx) >= (rect.right / 3)) {
-					KillTimer(hWnd, 3);
-					is_moving = false;
-					pieces_3x3[p.second][p.first - 1]->m_hx += 1;
-					pieces_3x3[p.second][p.first - 1]->m_dx = 0;
-					pieces_3x3[p.second][p.first] = pieces_3x3[p.second][p.first - 1];
-					pieces_3x3[p.second][p.first - 1] = nullptr;
-					--p.first;
+				is_moving = true;
+
+				pieces[iter->second][iter->first - 1]->m_dx += ((rect.right / col) / 16);
+
+				if (abs(pieces[iter->second][iter->first - 1]->m_dx) >= (rect.right / col)) {
+					pieces[iter->second][iter->first - 1]->m_hx += 1;
+					pieces[iter->second][iter->first - 1]->m_dx = 0;
+					pieces[iter->second][iter->first] = pieces[iter->second][iter->first - 1];
+					pieces[iter->second][iter->first - 1] = nullptr;
+					
+					iter->first -= 1;
+					empty_spaces.emplace_back(*iter);
+
+					iter = moving_spaces.erase(iter);
+					continue;
+				}
+				++iter;
+			}
+
+			if (!moving_spaces.size()) {
+				KillTimer(hWnd, 3);
+				is_moving = false;
+
+				if (check_win()) {
+					InvalidateRect(hWnd, NULL, TRUE);
+					MessageBox(hWnd, L"Game Clear!", L"You Win!", MB_OK | MB_ICONINFORMATION);
+					PostQuitMessage(0);
+					return 0;
 				}
 			}
+			break;
+
+		case 99:
+			hint = false;
 			break;
 		}
 		InvalidateRect(hWnd, NULL, TRUE);
 		break;
 
-
 	case WM_COMMAND:
 		switch (LOWORD(wParam)) {
+		case ID_PICTURE_1:
+			hBitmap = (HBITMAP)LoadImage(g_hInst, TEXT("kpu.bmp"), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+			GetObject(hBitmap, sizeof(BITMAP), &bmp);
+			set_pieces(row, col);
+			break;
+
+		case ID_PICTURE_2:
+			hBitmap = (HBITMAP)LoadImage(g_hInst, TEXT("tukorea.bmp"), IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION);
+			GetObject(hBitmap, sizeof(BITMAP), &bmp);
+			set_pieces(row, col);
+			break;
+
+		case ID_DIVIDE_3:
+			vh = false;
+			set_pieces(3, 3);
+			break;
+
+		case ID_DIVIDE_4:
+			vh = false;
+			set_pieces(4, 4);
+			break;
+
+		case ID_DIVIDE_5:
+			vh = false;
+			set_pieces(5, 5);
+			break;
+
+		case ID_GAME_START: {
+			start_game();
+			break;
+		}
+
+		case ID_GAME_HINT:
+			hint = true;
+			SetTimer(hWnd, 99, 1000, NULL);
+			break;
+
+		case ID_GAME_REVERSE:
+			reverse = !reverse;
+			break;
+
+		case ID_GAME_QUIT:
+			PostQuitMessage(0);
+			return 0;
 		}
 		InvalidateRect(hWnd, NULL, TRUE);
 		break;
 
 	case WM_CHAR:
 		switch (wParam) {
+		case 's':
+			start_game();
+			break;
+
+		case 'f':
+			hint = true;
+			SetTimer(hWnd, 99, 1000, NULL);
+			break;
+
+		case 'v':
+			vh = true;
+			set_pieces(1, max(row, col));
+			break;
+
+		case 'h':
+			vh = true;
+			set_pieces(max(row, col), 1);
+			break;
+
 		case 'q':
 			PostQuitMessage(0);
 			return 0;
@@ -266,74 +501,163 @@ LRESULT WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
 		break;
 
 	case WM_LBUTTONDOWN: {
-		clicked = true;
-		clicked_x = LOWORD(lParam);
-		clicked_y = HIWORD(lParam);
+		if (!vh) {
+			clicked = true;
+			clicked_x = LOWORD(lParam);
+			clicked_y = HIWORD(lParam);
+		} else {
+			clicked = true;
+			clicked_x = LOWORD(lParam);
+			clicked_y = HIWORD(lParam);
 
+			int x = clicked_x / (rect.right / col);
+			int y = clicked_y / (rect.bottom / row);
+			
+			if (pieces[y][x]) {
+				empty_spaces.emplace_back(std::make_pair(x, y));
+				picked = pieces[y][x];
+			}
+		}
 		InvalidateRect(hWnd, NULL, TRUE);
 		break;
 	}
 
 	case WM_LBUTTONUP:
-		clicked = false;
+		if (!vh) {
+			clicked = false;
+			clicked_x = LOWORD(lParam);
+			clicked_y = HIWORD(lParam);
+		} else {
+			if (picked) {
+				int new_x = clicked_x / (rect.right / col);
+				int new_y = clicked_y / (rect.bottom / row);
+				
+				int old_x = empty_spaces.begin()->first;
+				int old_y = empty_spaces.begin()->second;
+
+				if ((new_x != old_x) || (new_y != old_y)) {
+					if (pieces[new_y][new_x]) {
+						PIECE piece(pieces[old_y][old_x]->m_mx, pieces[old_y][old_x]->m_my);
+
+						pieces[old_y][old_x]->m_mx = pieces[new_y][new_x]->m_mx;
+						pieces[old_y][old_x]->m_my = pieces[new_y][new_x]->m_my;
+
+						picked->m_dx = 0; picked->m_dy = 0;
+						pieces[new_y][new_x]->m_mx = piece.m_mx;
+						pieces[new_y][new_x]->m_my = piece.m_my;
+
+						empty_spaces.clear();
+						picked = nullptr;
+					}
+				} else {
+					picked->m_dx = 0; picked->m_dy = 0;
+
+					empty_spaces.clear();
+					picked = nullptr;
+				}
+			}
+
+			if (check_win()) {
+				InvalidateRect(hWnd, NULL, TRUE);
+				MessageBox(hWnd, L"Game Clear!", L"You Win!", MB_OK | MB_ICONINFORMATION);
+				PostQuitMessage(0);
+				return 0;
+			}
+
+			InvalidateRect(hWnd, NULL, TRUE);
+		}
 		break;
 
 	case WM_MOUSEMOVE: {
 		if (!clicked || is_moving) { break; }
 
-		int x = LOWORD(lParam);
-		int y = HIWORD(lParam);
+		if (!vh) {
+			int x = LOWORD(lParam);
+			int y = HIWORD(lParam);
 
-		if (-50 > (y - clicked_y)) {
-			is_moving = true;
-			SetTimer(hWnd, 0, 16, NULL);
-			break;
+			if (-(HEIGHT / 2) > (y - clicked_y)) {
+				for (auto& p : empty_spaces) {
+					moving_spaces.emplace_back(p);
+				}
+				empty_spaces.clear();
+				SetTimer(hWnd, 0, 16, NULL);
+				break;
+			}
+
+			if ((HEIGHT / 2) < (y - clicked_y)) {
+				for (auto& p : empty_spaces) {
+					moving_spaces.emplace_back(p);
+				}
+				empty_spaces.clear();
+				SetTimer(hWnd, 1, 16, NULL);
+				break;
+			}
+
+			if (-(WIDTH / 2) > (x - clicked_x)) {
+				for (auto& p : empty_spaces) {
+					moving_spaces.emplace_back(p);
+				}
+				empty_spaces.clear();
+				SetTimer(hWnd, 2, 16, NULL);
+				break;
+			}
+
+			if ((WIDTH / 2) < (x - clicked_x)) {
+				for (auto& p : empty_spaces) {
+					moving_spaces.emplace_back(p);
+				}
+				empty_spaces.clear();
+				SetTimer(hWnd, 3, 16, NULL);
+				break;
+			}
+		} else {
+			if (picked) {
+				int x = LOWORD(lParam);
+				int y = HIWORD(lParam);
+
+				picked->m_dx += x - clicked_x;
+				picked->m_dy += y - clicked_y;
+
+				clicked_x = x;
+				clicked_y = y;
+
+				InvalidateRect(hWnd, NULL, TRUE);
+			}
 		}
-
-		if (50 < (y - clicked_y)) {
-			is_moving = true;
-			SetTimer(hWnd, 1, 16, NULL);
-			break;
-		}
-
-		if (-50 > (x - clicked_x)) {
-			is_moving = true;
-			SetTimer(hWnd, 2, 16, NULL);
-			break;
-		}
-
-		if (50 < (x - clicked_x)) { 
-			is_moving = true;
-			SetTimer(hWnd, 3, 16, NULL);
-			break;
-		}
-
 		break;
 	}
 
 	case WM_RBUTTONDOWN: {
 		if (is_moving) { break; }
 		
-		int cx = LOWORD(lParam) / (rect.right / 3);
-		int cy = HIWORD(lParam) / (rect.bottom / 3);
+		int cx = LOWORD(lParam) / (rect.right / col);
+		int cy = HIWORD(lParam) / (rect.bottom / row);
 
-		if (pieces_3x3[cy][cx]) {
-			removed_pieces.push(pieces_3x3[cy][cx]);
+		if (pieces[cy][cx]) {
+			removed_pieces.push(pieces[cy][cx]);
 			empty_spaces.emplace_back(std::make_pair(cx, cy));
-			pieces_3x3[cy][cx] = nullptr;
-		}
-		else {
+			pieces[cy][cx] = nullptr;
+		} else {
 			if (!removed_pieces.empty()) {
 				PIECE* p = removed_pieces.front();
 				p->m_hx = cx;
 				p->m_hy = cy;
-				pieces_3x3[cy][cx] = p;
+				pieces[cy][cx] = p;
 				auto iter = std::find_if(empty_spaces.begin(), empty_spaces.end(), [&cx, &cy](const auto& p) {
 					return (p.first == cx) && (p.second == cy);
 					});
 				empty_spaces.erase(iter);
 				removed_pieces.pop();
+			} else {
+				MessageBox(hWnd, L"Can't Place!", L"Error", MB_ICONERROR);
 			}
+		}
+
+		if (check_win()) {
+			InvalidateRect(hWnd, NULL, TRUE);
+			MessageBox(hWnd, L"Game Clear!", L"You Win!", MB_OK | MB_ICONINFORMATION);
+			PostQuitMessage(0);
+			return 0;
 		}
 
 		InvalidateRect(hWnd, NULL, TRUE);
@@ -346,10 +670,29 @@ LRESULT WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
 
 		SelectObject(mDC, (HBITMAP)hBitmap);
 
-		for (const auto& row : pieces_3x3) {
-			for (const auto& piece : row) {
-				if (nullptr == piece) { continue; }
-				piece->print(hDC, mDC, rect, bmp, 3);
+		if (!hint) {
+			if (!reverse) {
+				for (const auto& r : pieces) {
+					for (const auto& piece : r) {
+						if (nullptr == piece) { continue; }
+						piece->print(hDC, mDC, rect, bmp, row, col);
+					}
+				}
+			} else {
+				for (const auto& r : pieces) {
+					for (const auto& piece : r) {
+						if (nullptr == piece) { continue; }
+						piece->print(hDC, mDC, rect, bmp, row, col, true);
+					}
+				}
+			}
+		} else {
+			if (!reverse) {
+				StretchBlt(hDC, 0, 0, rect.right, rect.bottom,
+					mDC, 0, 0, bmp.bmWidth, bmp.bmHeight, SRCCOPY);
+			} else {
+				StretchBlt(hDC, 0, 0, rect.right, rect.bottom,
+					mDC, 0, 0, bmp.bmWidth, bmp.bmHeight, NOTSRCCOPY);
 			}
 		}
 
--- 실습 5-2/resource.h
+++ 실습 5-2/resource.h
@@ -1,13 +1,24 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by Project.rc
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Project.rc에서 사용되고 있습니다.
+//
+#define IDR_MENU1                       101
+#define ID_PICTURE_1                    40001
+#define ID_PICTURE_2                    40002
+#define ID_DIVIDE_3                     40003
+#define ID_DIVIDE_4                     40004
+#define ID_DIVIDE_5                     40005
+#define ID_GAME_START                   40006
+#define ID_GAME_HINT                    40007
+#define ID_GAME_REVERSE                 40008
+#define ID_GAME_QUIT                    40009
 
-// 다음은 새 개체에 사용할 기본값입니다.
+// Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
-#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40010
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
